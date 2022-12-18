c13800a5e59fcc3cc0b43aac9353fc79fc013258
Kokezer <101249226+Kokezer@users.noreply.github.com>
2022-12-18 21:30:36 +0300

    Add files via upload

---
+++ prac2/src/pract_2/zd10/HowMany.java
@@ -0,0 +1,28 @@
+package pract_2.zd10;
+
+public class HowMany {
+    private String string;
+    private int wordSum;
+
+    HowMany(String string){
+        this.string=string;
+    }
+
+    private void wordFinding(){
+        int localeSum=0;
+        for(int i = 1; i<this.string.length()-1;i++){
+            if(this.string.charAt(i-1)==' '&& this.string.charAt(i+1)!=' '){
+                localeSum++;
+            }
+        }
+        setWordSum(localeSum);
+    }
+
+    public void setWordSum(int wordSum) {
+        this.wordSum = wordSum;
+    }
+
+    public int getWordSum() {
+        return wordSum;
+    }
+}
---
+++ prac2/src/pract_2/zd9/Pk.java
@@ -0,0 +1,11 @@
+package pract_2.zd9;
+
+import java.util.Scanner;
+
+public class Pk {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
