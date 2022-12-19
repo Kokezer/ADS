13e976b904f215e59704ea3761ebba672670ba31
Kokezer <101249226+Kokezer@users.noreply.github.com>
2022-12-19 10:52:34 +0300

    Add files via upload

---
+++ Praс4.1/src/zd2/prac4_1/testPhone.java
@@ -0,0 +1,8 @@
+package zd2.prac4_1;
+
+public class testPhone {
+    public static void main(String[] args) {
+        Phone phone = new Phone("iPhone", "8-913-375-57-67", 0.2);
+        phone.receiveCall("Pasha");
+    }
+}
---
+++ Praс4.1/src/zd8/prac4_1/tet.java
@@ -0,0 +1,7 @@
+package zd8.prac4_1;
+
+public class tet {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+    }
+}
